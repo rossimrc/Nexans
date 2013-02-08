fd674d9fadce01a745f2c06333b57968e6a5c9d2
cavalarit <thiago.cavalari@gmail.com>
2013-02-08 17:53:41 -0200

    Revert "restante projeto(view)"
    This reverts commit c13231db15919a122649bc4a0f89597df02c8a53.

---
+++ CabosIOS6/www/js/about.js
@@ -0,0 +1,22 @@
+var app = {
+    initialize: function() {
+        this.bind();
+        document.body.addEventListener('touchmove', function(e) {
+			e.preventDefault();
+		}, false);
+		$("#return-btn").click(function(){
+			history.back();
+		});
+    },
+    bind: function() {
+        document.addEventListener('deviceready', this.deviceready, false);
+    },
+    deviceready: function() {
+        // note that this is an event handler so the scope is that of the event
+        // so we need to call app.report(), and not this.report()
+        app.report('deviceready');
+    },
+    report: function(id) { 
+        console.log("report:" + id);
+    }
+};
